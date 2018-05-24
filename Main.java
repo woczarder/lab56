72cc64385b9fd771e582370f8e39056bfdf356e4
woczarder <wojtasessek1997@gmail.com>
2018-05-24 19:11:28 +0200

    Działa factorial. Wow.

--- Main.java
+++ Main.java
@@ -2,16 +2,13 @@ package dev.harddrillstudio.wat.lab56;
 
 public class Main {
 
-    public static void main(String[] args) {
-        DoTrzechRazySztuka xxx = new DoTrzechRazySztuka();
+    public static void main(String[] args) throws Exception {
+        Factorial factorial = new Factorial();
 
-        for (int i = 0; i < 3; i++) {
-            try {
-                System.out.println(xxx.readFloat());
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println("JESTES GEJEM");
-            }
+        try {
+            System.out.println(factorial.factorial(4));
+        } catch (IllegalArgumentException e) {
+            System.out.println("liczba mniejsza od zera, geju");
         }
 
     }
